4d5075b5cef1357c95bc3072d4aa400bc3128e9d
Andrey <Andreyshabalin34@gmail.com>
2022-07-26 17:14:20 +0600

    fix param lang

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -11,12 +11,9 @@ class BasketPage(BasePage):
 
 
     def is_basket_empty(self):
-        basket_caption = self.browser.find_element(*BasketPageLocators.BASKET_CAPTION)
-        basket_text = basket_caption.text
         try:
             basket_inner = self.browser.find_element(*BasketPageLocators.BASKET_INNER)
             isPresent = False
         except(NoSuchElementException):
             isPresent = True
-        time.sleep(10)
-        assert ("Your basket is empty" in (basket_text)) and isPresent, "Basket is not empty"
+        assert (isPresent), "Basket is not empty"
--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,4 @@
+from pages.main_page import MainPage
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 import pytest
@@ -5,15 +6,21 @@ import pytest
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"])
 def test_guest_can_go_to_login_page(browser,link):
+    """
+    Проверка различных урлов на возможность открытия и добавление в корзину товаров
+    """
     page = ProductPage(browser,link)
     page.open()
     page.add_to_cart_page()
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    """
+    Проверка чтобы успешное сообщение не появлялось при добавлении продукта в корзину для гостя сайта
+    """
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
@@ -22,6 +29,9 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
 
 
 def test_guest_cant_see_success_message(browser):
+    """
+    Проверка чтобы успешное сообщение не появлялось при открытии страницы для гостя сайта
+    """
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
@@ -29,6 +39,9 @@ def test_guest_cant_see_success_message(browser):
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
+    """
+    Проверка исчезновения сообщения после добавления продукта в корзину
+    """
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)
     page.open()
@@ -36,6 +49,9 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_disappeared()
 
 def test_guest_should_see_login_link_on_product_page(browser):
+    """
+    Проверка наличия ссылки авторизации для гостя
+    """
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
@@ -43,12 +59,18 @@ def test_guest_should_see_login_link_on_product_page(browser):
 
 
 def test_guest_can_go_to_login_page_from_product_page(browser):
+    """
+    Проверка возможности перехода на страницу авторизации со страницы продукта для гостя
+    """
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    """
+    Проверка отображения продукта в корзине, открытой из страницы продукта для  гостя(Отрицательнная проверка)
+    """
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
     page.open()
@@ -56,7 +78,10 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page.is_basket_empty()
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/"
+    """
+    Проверка отображения продукта в корзине, открытой из главной страницы продукта для гостя (Отрицательнная проверка)
+    """
+    link = "http://selenium1py.pythonanywhere.com/"
     page = BasketPage(browser, link)
     page.open()
     page.go_to_basket()
